71a5cb590541282ecced80319a5c3e16c99d932d
marcosborg <m.borges.mail@gmail.com>
2025-05-20 07:53:21 +0100

    Update vehicle usage view to include monthly tabs with corresponding images

--- resources/views/admin/vehicleUsages/usage.blade.php
+++ resources/views/admin/vehicleUsages/usage.blade.php
@@ -13,20 +13,26 @@
 
                         <!-- Nav tabs -->
                         <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
-                            <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
-                            <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Messages</a></li>
-                            <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
+                            <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Fevereiro</a></li>
+                            <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Março</a></li>
+                            <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Abril</a></li>
+                            <li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Maio</a></li>
                         </ul>
 
                         <!-- Tab panes -->
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane active" id="home">
-                                <img src="/assets/admin/img/fevereiro.pmg" alt="">
+                                <img src="https://tribosemontanhas.gestvde.pt/assets/admin/img/fevereiro.png" class="img-responsive">
                             </div>
-                            <div role="tabpanel" class="tab-pane" id="profile">2..</div>
-                            <div role="tabpanel" class="tab-pane" id="messages">...</div>
-                            <div role="tabpanel" class="tab-pane" id="settings">...</div>
+                            <div role="tabpanel" class="tab-pane" id="profile">
+                                <img src="https://tribosemontanhas.gestvde.pt/assets/admin/img/marco.png" class="img-responsive">
+                            </div>
+                            <div role="tabpanel" class="tab-pane" id="messages">
+                                <img src="https://tribosemontanhas.gestvde.pt/assets/admin/img/abril.png" class="img-responsive">
+                            </div>
+                            <div role="tabpanel" class="tab-pane" id="settings">
+                                <img src="https://tribosemontanhas.gestvde.pt/assets/admin/img/maio.png" class="img-responsive">
+                            </div>
                         </div>
 
                     </div>
