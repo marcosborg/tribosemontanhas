6c12bb21e530ee2a1a4d468be85835f5b22a23eb
marcosborg <m.borges.mail@gmail.com>
2025-07-15 09:08:55 +0100

    Update driver balance display logic to show correct transit saldo based on balance conditions

--- resources/views/admin/financialStatements/index.blade.php
+++ resources/views/admin/financialStatements/index.blade.php
@@ -141,8 +141,10 @@
                             </tr>
                         </tbody>
                     </table>
-                    @if ($driver_balance)
+                    @if ($driver_balance && $driver_balance->drivers_balance > 0)
                     <p><small>Saldo transitado: {{ number_format($total - $driver_balance->drivers_balance ?? 0, 2) }}€</small></p>
+                    @else
+                    <p><small>Saldo transitado: {{ number_format($total + $driver_balance->drivers_balance ?? 0, 2) }}€</small></p>
                     @endif
                 </div>
             </div>
